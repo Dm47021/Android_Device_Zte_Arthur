3c7677a399007c48e15f59c911244848b880e27e
dm47021 <dm47021@hotmail.com>
2014-01-04 19:58:46 -0500

    update wifi to be inline with cm10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,22 +155,18 @@ COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DREFRESH_RATE=60 -DQCOM_LEGACY_OMX
 
 # Wifi
 # Defines for external/wpa_supplicant_*
-BOARD_WLAN_DEVICE := qcwcn
 BOARD_HAS_QCOM_WLAN := true
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-HOSTAPD_VERSION := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/libra.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
-WIFI_DRIVER_MODULE_ARG := ""
-WIFI_EXT_MODULE_PATH := "/system/lib/modules/librasdioif.ko"
-WIFI_EXT_MODULE_NAME := "librasdioif"
-WIFI_PRE_LOADER := "qcom_sdio_init"
-WIFI_POST_UNLOADER := "qcom_sdio_deinit"
 
+WPA_SUPPLICANT_VERSION       := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER  := WEXT
+BOARD_WLAN_DEVICE            := libra
+WIFI_DRIVER_MODULE_PATH      := /system/lib/modules/libra.ko
+WIFI_DRIVER_MODULE_NAME      := libra
+WIFI_EXT_MODULE_PATH         := /system/lib/modules/librasdioif.ko
+WIFI_EXT_MODULE_NAME         := librasdioif
+BOARD_WEXT_NO_COMBO_SCAN := true
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WPA_BUILD_SUPPLICANT := true
 
 # Bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
